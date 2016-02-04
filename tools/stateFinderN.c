60ade1b68278a08933d536e104b74f332579b7bb
root <benny.saxen@gmail.com>
2016-02-04 20:24:55 +0100

    imp

--- tools/stateFinderN.c
+++ tools/stateFinderN.c
@@ -23,9 +23,13 @@ float g_pmin,g_pmax;
 int n_trans = 0;
 int n_state = 0;
 int n_data = 0;
+
 //================================
-float fSub = 1.;
-int thresholdA = 1;
+float fSub = 50.;
+int thresholdA = 50;
+int g_filter = 0;
+//float fSub = 1.;
+//int thresholdA = 1;
 //================================
 int nSub;
 int g_nfreq[DATA_MAX], g_time[DATA_MAX];
@@ -41,7 +45,14 @@ void init()
 //===========================================
 {
 	int i;
-	for(i=0;i<1000;i++) g_nfreq[i] = 0;
+	for(i=0;i<DATA_MAX;i++) 
+    {
+        g_nfreq[i] = 0;
+        g_time[i] = 0;
+        g_state[i] = 0;
+        state_transition_from[i] = 0;
+        state_transition_to[i] = 0;
+    }
 }
 
 //===========================================
@@ -50,16 +61,17 @@ void stateFinderFunc()
 {
    int i,j;
    float left,right;
-   n_data = lib_readNilmFile(1,"autoGraph.nilm");
+   n_data = lib_readNilmFile(1,"../../nytomta/data-302-2016-01-13.nbc");
+   //n_data = lib_readNilmFile(1,"autoGraph.nilm");
    g_pmax = g_ymax;
    g_pmin = g_ymin; 
 
    nSub = (int)((g_pmax - g_pmin)/fSub);
-   printf("min=%f max=%f\n",g_pmin,g_pmax);
-   printf("Number of data = %d fSub=%f nSub=%d\n",n_data,fSub,nSub);
+   //printf("min=%f max=%f\n",g_pmin,g_pmax);
+
 
    float subInt = fSub;//(g_pmax- g_pmin)/nSub;
-   printf("subInt=%f\n",subInt);
+   //printf("subInt=%f\n",subInt);
 
    for(i=0;i<=nSub;i++)
    {
@@ -80,10 +92,11 @@ void stateFinderFunc()
          k++;
          g_state[k] = ftemp;
          float diff = ftemp - g_state[k-1];
-         printf("state=%3d sub=%3d freq=%3d value=%5.1f diff=%5.1f\n",k,i,g_nfreq[i],ftemp,diff);
+         //printf("state=%3d sub=%3d freq=%3d value=%5.1f diff=%5.1f\n",k,i,g_nfreq[i],ftemp,diff);
      }
    }
    n_state = k;
+   printf("Data = %d fSub=%f threshold=%d filter=%d => states=%d\n",n_data,fSub,thresholdA,g_filter,n_state);
 }
 //===========================================
 int getState(float x)
@@ -93,15 +106,15 @@ int getState(float x)
     for(i=1;i<=n_state;i++)
     {
         //printf("%d %f %f\n",i,x,g_state[i]);
-        if(abs(g_state[i] - x) <= 0.5 )
+        if(abs(g_state[i] - x) <= fSub )
         {
             j++;
             res = i;
             //printf("**** %d %f %f\n",i,x,g_state[i]);
         }
     }
-    if(j==0)printf("No state found for %f\n",x);
-    if(j>1)printf("Multiple states found for %f\n",x);
+    //if(j==0)printf("No state found for %f\n",x);
+    //if(j>1)printf("Multiple(%d) states found for %f\n",j,x);
     //printf("Number of states %d\n",g_k);
     return(res);
 }
@@ -123,7 +136,7 @@ void stateSeqFinderFunc()
             state_transition_to[k] = st;
         }
     }
-    printf("State transitions = %d\n",k);
+    //printf("State transitions = %d\n",k);
     n_trans = k;
 }
 //===========================================
@@ -162,7 +175,7 @@ void stateFreqFinderFunc()
         {
             ftemp = (g_state[i]-g_state[j])/10;
             ftemp = abs(ftemp);
-            if(M[i][j] > 30)printf("%3.0f ",ftemp);
+            if(M[i][j] > g_filter)printf("%3.0f ",ftemp);
             else 
                 printf("    ");
         }
@@ -175,9 +188,40 @@ main()
 //===========================================
 //===========================================
 {
-   init();
-   stateFinderFunc();
-   stateSeqFinderFunc();
-   stateFreqFinderFunc();
+    int ch;
+
+    while(1)  
+    {
+        init();
+        stateFinderFunc();
+        stateSeqFinderFunc();
+        stateFreqFinderFunc();
+        ch = getchar();
+        if (ch=='a')
+	    {
+            fSub++;
+	    }
+        else if (ch=='z')
+        {
+            fSub--;
+        }
+        else if (ch=='s')
+        {
+            thresholdA++;
+        }
+        else if (ch=='x')
+        {
+            thresholdA--;
+        }
+        else if (ch=='d')
+        {
+            g_filter++;
+        }
+        else if (ch=='c')
+        {
+            g_filter--;
+        }
+
+      }
 }
 // End of file
