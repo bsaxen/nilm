cac2083d229518f2df1eeba0b2eda23d2355a12a
bsaxen <benny.saxen@gmail.com>
2017-04-17 11:06:55 +0200

    Update extractIoantData.php

--- extractIoantData.php
+++ extractIoantData.php
@@ -1,15 +1,20 @@
 <?php
-$username="root";
-$password="pswd";
-$database="ioant";
-$field1-name=$_POST['Value1'];
-$field2-name=$_POST['Value2'];
-$field3-name=$_POST['Value3'];
-$field4-name=$_POST['Value4'];
-$field5-name=$_POST['Value5'];
-mysql_connect(localhost,$username,$password);
-@mysql_select_db($database) or die( "Unable to select database");
-$query = "SELECT * FROM stream_49_electricpower WHERE ts BETWEEN('2017-01-16 00:00:00' AND '2017-01-16 23:59:59' ORDER BY datetime DESC ");
-mysql_query($query);
-mysql_close();
+$con=mysqli_connect("localhost","root","my_password","my_db");
+
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$sql="SELECT * FROM stream_49_electricpower WHERE ts BETWEEN('2017-01-16 00:00:00' AND '2017-01-16 23:59:59' ORDER BY ts DESC ");
+
+if ($result=mysqli_query($con,$sql))
+{
+  while ($row=mysqli_fetch_row($result))
+  {
+    printf ("%s (%s)\n",$row[0],$row[1]);
+  }
+  mysqli_free_result($result);
+}
+mysqli_close($con);
 ?>
