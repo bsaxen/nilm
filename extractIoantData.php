13d1686cdee7cd8a40dcae1399c3a4cdd717cedf
bsaxen <benny.saxen@gmail.com>
2017-04-17 11:32:17 +0200

    Update extractIoantData.php

--- extractIoantData.php
+++ extractIoantData.php
@@ -1,20 +1,25 @@
 <?php
-$con=mysqli_connect("localhost","root","my_password","my_db");
+$con=mysqli_connect("localhost","root","mypassword","mydb");
 
 if (mysqli_connect_errno())
 {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+echo "Connected to Database";
 
-$sql="SELECT * FROM stream_49_electricpower WHERE ts BETWEEN('2017-01-16 00:00:00' AND '2017-01-16 23:59:59' ORDER BY ts DESC ");
+$sql="SELECT * FROM stream_49_electricpower WHERE ts BETWEEN TIMESTAMP('2017-01-16 00:00:00') AND TIMESTAMP('2017-01-16 23:59:59') ORDER BY ts ASC ";
+
+echo $sql;
 
 if ($result=mysqli_query($con,$sql))
 {
-  while ($row=mysqli_fetch_row($result))
+  while ($row=mysqli_fetch_assoc($result))
   {
-    printf ("%s (%s)\n",$row[0],$row[1]);
+    printf ("%s %s\n",$row['ts'],$row['value']);
   }
   mysqli_free_result($result);
 }
+echo "Close Database";
 mysqli_close($con);
 ?>
+
